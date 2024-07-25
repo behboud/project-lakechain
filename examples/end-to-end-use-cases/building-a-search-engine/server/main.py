df3dc008e1a928d360b1eca7f9726f40e1639286
Behboud Kalantary <behboud@amazon.de>
2024-07-25 07:39:05 +0000

    feat: api /embedding added

--- examples/end-to-end-use-cases/building-a-search-engine/server/main.py
+++ examples/end-to-end-use-cases/building-a-search-engine/server/main.py
@@ -12,26 +12,18 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from urllib.parse import unquote
+
+from embeddings.image_embeddings import clip_create_text_embeddings
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from utils import presign_url
-from urllib.parse import unquote
 from mangum import Mangum
-
-from params import (
-  es_hostname,
-  es_port,
-  aws_region
-)
-
-from opensearch import (
-  create_opensearch_client,
-  text_semantic_search,
-  image_semantic_search,
-  delete_documents,
-  delete_images
-)
+from opensearch import (create_opensearch_client, delete_documents,
+                        delete_images, image_semantic_search,
+                        text_semantic_search)
+from params import aws_region, es_hostname, es_port
+from utils import presign_url
 
 # Create the FastAPI app.
 app = FastAPI()
@@ -44,76 +36,98 @@ templates = Jinja2Templates(directory="templates")
 
 # Create the OpenSearch client.
 client = create_opensearch_client(
-  endpoint=es_hostname,
-  port=es_port,
-  region_name=aws_region
+    endpoint=es_hostname,
+    port=es_port,
+    region_name=aws_region
 )
+
 
 @app.get("/")
 def index(request: Request):
-  """
-  Renders the index page.
-  :param request: The HTTP request.
-  """
-  return templates.TemplateResponse("index.html", {"request": request})
+    """
+    Renders the index page.
+    :param request: The HTTP request.
+    """
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/search")
 def search_documents(request: Request):
-  """
-  Performs a semantic search on documents given the search text.
-  :param request: The HTTP request.
-  """
-  query = request.query_params.get('q')
+    """
+    Performs a semantic search on documents given the search text.
+    :param request: The HTTP request.
+    """
+    query = request.query_params.get('q')
 
-  # Text semantic search.
-  results = text_semantic_search(client=client, search=query)
+    # Text semantic search.
+    results = text_semantic_search(client=client, search=query)
 
-  # Add a presigned URL to the results and
-  # extract metadata from the results.
-  for result in results:
-    source = result['source']
-    metadata = result['metadata']
-    source['presigned_url'] = presign_url(s3_url=source['url'], region=aws_region)
-    source['title'] = metadata['title'] if 'title' in metadata else unquote(source['url'].split('/')[-1])
+    # Add a presigned URL to the results and
+    # extract metadata from the results.
+    for result in results:
+        source = result['source']
+        metadata = result['metadata']
+        source['presigned_url'] = presign_url(
+            s3_url=source['url'], region=aws_region)
+        source['title'] = metadata['title'] if 'title' in metadata else unquote(
+            source['url'].split('/')[-1])
 
-  # Render the results.
-  return templates.TemplateResponse("search.html", {"request": request, "results": results})
+    # Render the results.
+    return templates.TemplateResponse("search.html", {"request": request, "results": results})
 
 
 @app.get("/search/images")
 def search_images(request: Request):
-  """
-  Performs a semantic search for images given the search text.
-  :param request: The HTTP request.
-  """
-  query = request.query_params.get('q')
+    """
+    Performs a semantic search for images given the search text.
+    :param request: The HTTP request.
+    """
+    query = request.query_params.get('q')
 
-  # Image semantic search.
-  results = image_semantic_search(client=client, search=query)
+    # Image semantic search.
+    results = image_semantic_search(client=client, search=query)
 
-  # Add a presigned URL to the results.
-  for result in results:
-    document = result['_source']['data']['document']
-    document['presigned_url'] = presign_url(s3_url=document['url'], region=aws_region)
+    # Add a presigned URL to the results.
+    for result in results:
+        document = result['_source']['data']['document']
+        document['presigned_url'] = presign_url(
+            s3_url=document['url'], region=aws_region)
 
-  # Render the results.
-  return templates.TemplateResponse("image-search.html", {"request": request, "results": results})
+    # Render the results.
+    return templates.TemplateResponse("image-search.html", {"request": request, "results": results})
 
 
 @app.delete("/documents")
 def delete_documents_from_index():
-  """
-  Deletes all documents from the index.
-  """
-  return delete_documents(client)
+    """
+    Deletes all documents from the index.
+    """
+    return delete_documents(client)
 
 
 @app.delete("/images")
 def delete_images_from_index():
-  """
-  Deletes all images from the index.
-  """
-  return delete_images(client)
+    """
+    Deletes all images from the index.
+    """
+    return delete_images(client)
+
+
+# post method to receive a string as part of the request body and return the CLIP embedding
+
+@app.post("/embedding")
+async def get_clip_embedding(request: Request):
+    """
+    Returns the CLIP embedding for the given text.
+    :param request: The HTTP request.
+    """
+    text = await request.body()
+    text = text.decode('utf-8')
+    # Call the get_clip_embedding function with the text
+    # and return the embedding
+    # TODO: raises runtime error for too long context length
+    embedding = clip_create_text_embeddings(text)
+    return {"embedding": embedding}
+
 
 handler = Mangum(app)
